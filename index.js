e590022205e3907a1b23f939dfe4fca961fe05a5
bla211 <brad.louis.alexander@gmail.com>
2018-07-31 16:55:31 -0400

    refactored to use revealing modules

--- index.js
+++ index.js
@@ -1,140 +1,163 @@
-var etsy = etsy || {};
-etsy.apiKey = '2z5arsdpfe5py1mcc5welkm2';
-etsy.keywords = '';
-etsy.searchResults = [];
-etsy.numberOfResults = null;
-etsy.savedSearches = [];
+var etsy = (() => {
+  const apiKey = '2z5arsdpfe5py1mcc5welkm2';
+  let keywords = '';
+  let searchResults = [];
+  let numberOfResults = null;
+  let savedSearches = [];
 
-//callback for jsonp
+  search = (type) => {
+    if(type === 'click' || type === 'load'){
+        clearSearchResults();
+        showLoadingAnimation();
+    }
+    document.getElementById('load_next').style.opacity = 0;
+    keywords = document.getElementById('search_box').value;
+    let script = document.createElement('script');
+    script.src = generateScript(type, keywords, searchResults.length);
+    document.getElementsByTagName('head')[0].appendChild(script);
+  };
+
+  clearSearchResults = () => {
+    searchResults = [];
+    document.getElementById('results_list').innerHTML = ''
+  }
+
+  showLoadingAnimation = () => {
+    document.getElementById('loading_animation').style.display = 'block';
+  };
+
+  hideLoadingAnimation = () => {
+    document.getElementById('loading_animation').style.display = 'block';
+  };
+
+  generateScript = (type, keywords, resultsLength) => {
+    let keywordString = '';
+    let offsetString = '';
+    if(type === 'click'){
+      keywordString = '&keywords=' + encodeURI(keywords);
+    }
+    else if(type === 'next'){
+        if(keywords.length){
+            keywordString = '&keywords=' + encodeURI(keywords);
+        }
+        offsetString = '&offset=' + resultsLength;
+    }
+    return 'https://openapi.etsy.com/v2/listings/active.js?includes=MainImage&callback=handleData&api_key=' + apiKey + keywordString + offsetString + '&limit=25';
+  };
+
+  handleResults = (data) => {
+    const results = data.results
+    numberOfResults = data.count;
+    results.forEach((val) => {
+      searchResults.push(val);
+    });
+    buildList();
+  };
+
+  buildList = () => {
+    let html = '';
+    searchResults.forEach((result, index) => {
+      if(result.MainImage){
+        html += generateResultsHTML(result, index);
+      }
+    });
+
+    document.getElementById('results_list').innerHTML = html;
+    document.getElementById('results_count').innerHTML = resultsCount(searchResults, numberOfResults);
+    document.getElementById('load_next').style.opacity = 1;
+  };
+
+  generateResultsHTML = (result, index) => {
+    let html = '';
+    html += '<li data-index="' + index + '">';
+    html += '<div class="tile_thumb" style="background-image:url(' + result.MainImage.url_fullxfull + ')"></div>';
+    html += '<div class="price">$' + result.price + '</div>'
+    html += '<div class="tile_title" onclick="etsy.loadEtsyPage(' + index + ')">';
+    html += result.title;
+    html += '<div class="view_product">View Product</div>';
+    html += '</div>';
+    html += '</li>';
+    return html;
+  };
+
+  resultsCount = (searchResults, numberOfResults) => {
+    return 'Displaying ' + searchResults.length + ' of ' + numberOfResults + ' results';
+  };
+
+  handleEnterPress = () => {
+    document.getElementById('search_box').onkeydown = function(e){
+       if(e.keyCode == 13){
+         search('click');
+       }
+    };
+  };
+
+  loadEtsyPage = (index) => {
+    window.open(searchResults[index].url);
+  };
+
+  getSavedSearches = () => {
+      let savedSearchesJSON = JSON.parse(localStorage.getItem("savedSearches"));
+      savedSearches = savedSearchesJSON;
+
+      buildSavedSearchList();
+  };
+
+  buildSavedSearchList = () => {
+      let html = '';
+      if(savedSearches !== null){
+        savedSearches.forEach((val, i) => {
+          html += '<li onclick="etsy.loadSearch(' + i + ')">' + val + '</li>'
+        });
+
+        document.getElementById('saved_search_list').innerHTML = html;
+      }
+  };
+
+  saveSearch = () => {
+    //doing this in local storage in lieu of rest api
+    if(keywords.length){
+      let savedSearchesJSON = JSON.parse(localStorage.getItem("savedSearches"));
+      if(savedSearchesJSON === null){
+        savedSearchesJSON = [];
+      }
+      savedSearchesJSON.push(keywords);
+      localStorage.setItem("savedSearches", JSON.stringify(savedSearchesJSON));
+
+      savedSearches = savedSearchesJSON;
+      buildSavedSearchList();
+    }
+  };
+
+  loadSearch = (index) => {
+    keywords = savedSearches[index];
+    document.getElementById('search_box').value = keywords;
+    search('click');
+  };
+
+  let testObj = {
+    generateScript: generateScript,
+    generateResultsHTML: generateResultsHTML,
+    resultsCount: resultsCount
+  }
+
+  return {
+    search: search,
+    handleResults: handleResults,
+    loadSearch: loadSearch,
+    getSavedSearches: getSavedSearches,
+    handleEnterPress: handleEnterPress,
+    saveSearch: saveSearch,
+    loadEtsyPage: loadEtsyPage,
+    testObj: testObj
+  }
+})()
+
 handleData = (data) => {
   if (data.ok) {
-    etsy.numberOfResults = data.count;
-    etsy.handleResults(data.results);
+    etsy.handleResults(data);
   }
   else {
     console.log(data.error);
   }
-};
-
-etsy.search = (type) => {
-  if(type === 'click' || type === 'load'){
-      etsy.clearSearchResults();
-      etsy.showLoadingAnimation();
-  }
-  document.getElementById('load_next').style.opacity = 0;
-  etsy.keywords = document.getElementById('search_box').value;
-  let script = document.createElement('script');
-  script.src = etsy.generateScript(type, etsy.keywords, etsy.searchResults.length);
-  document.getElementsByTagName('head')[0].appendChild(script);
-};
-
-etsy.buildList = () => {
-  let html = '';
-  etsy.searchResults.forEach((result, index) => {
-    if(result.MainImage){
-      html += etsy.generateResultsHTML(result, index);
-    }
-  });
-
-  document.getElementById('results_list').innerHTML = html;
-  document.getElementById('results_count').innerHTML = etsy.resultsCount(etsy.searchResults, etsy.numberOfResults);
-  document.getElementById('load_next').style.opacity = 1;
-};
-
-etsy.handleResults = (results) => {
-  results.forEach((val) => {
-    etsy.searchResults.push(val);
-  });
-  etsy.buildList();
-};
-
-etsy.handleEnterPress = () => {
-  document.getElementById('search_box').onkeydown = function(e){
-     if(e.keyCode == 13){
-       etsy.search('click');
-     }
-  };
-};
-
-etsy.loadEtsyPage = (index) => {
-  window.open(etsy.searchResults[index].url);
-};
-
-etsy.getSavedSearches = () => {
-    let savedSearches = JSON.parse(localStorage.getItem("savedSearches"));
-    etsy.savedSearches = savedSearches;
-
-    etsy.buildSavedSearchList();
-};
-
-etsy.saveSearch = () => {
-  //doing this in local storage in lieu of rest api
-  if(etsy.keywords.length){
-    let savedSearches = JSON.parse(localStorage.getItem("savedSearches"));
-    if(savedSearches === null){
-      savedSearches = [];
-    }
-    savedSearches.push(etsy.keywords);
-    localStorage.setItem("savedSearches", JSON.stringify(savedSearches));
-
-    etsy.savedSearches = savedSearches;
-    etsy.buildSavedSearchList();
-  }
-};
-
-etsy.buildSavedSearchList = () => {
-    let html = '';
-    if(etsy.savedSearches !== null){
-      etsy.savedSearches.forEach((val, i) => {
-        html += '<li onclick="etsy.loadSearch(' + i + ')">' + val + '</li>'
-      });
-
-      document.getElementById('saved_search_list').innerHTML = html;
-    }
-};
-
-etsy.loadSearch = (index) => {
-  etsy.keywords = etsy.savedSearches[index];
-  document.getElementById('search_box').value = etsy.keywords;
-  etsy.search('click');
-};
-
-etsy.resultsCount = (searchResults, numberOfResults) => {
-  return 'Displaying ' + searchResults.length + ' of ' + numberOfResults + ' results';
-};
-
-etsy.generateScript = (type, keywords, resultsLength) => {
-  let keywordString = '';
-  let offsetString = '';
-  if(type === 'click'){
-    keywordString = '&keywords=' + encodeURI(keywords);
-  }
-  else if(type === 'next'){
-      if(keywords.length){
-          keywordString = '&keywords=' + encodeURI(keywords);
-      }
-      offsetString = '&offset=' + resultsLength;
-  }
-  return 'https://openapi.etsy.com/v2/listings/active.js?includes=MainImage&callback=handleData&api_key=' + etsy.apiKey + keywordString + offsetString + '&limit=25';
-};
-
-etsy.showLoadingAnimation = () => {
-  document.getElementById('results_list').innerHTML = '<img id="loading_animation" src="https://josephdoughertyblog.files.wordpress.com/2014/09/loading-animation-white.gif"/>';
-};
-
-etsy.clearSearchResults = () => {
- etsy.searchResults = [];
-};
-
-etsy.generateResultsHTML = (result, index) => {
-  let html = '';
-  html += '<li data-index="' + index + '">';
-  html += '<div class="tile_thumb" style="background-image:url(' + result.MainImage.url_fullxfull + ')"></div>';
-  html += '<div class="price">$' + result.price + '</div>'
-  html += '<div class="tile_title" onclick="etsy.loadEtsyPage(' + index + ')">';
-  html += result.title;
-  html += '<div class="view_product">View Product</div>';
-  html += '</div>';
-  html += '</li>';
-  return html;
 };
--- tests/EtsySpec.js
+++ tests/EtsySpec.js
@@ -1,7 +1,7 @@
 describe("Results count", function() {
   it("shows number of results shown out of total results", function() {
     const searchResults = [1,2,3,4,5,6,7]
-    expect(etsy.resultsCount(searchResults, 1000)).toEqual('Displaying 7 of 1000 results');
+    expect(etsy.testObj.resultsCount(searchResults, 1000)).toEqual('Displaying 7 of 1000 results');
   });
 });
 
@@ -10,7 +10,7 @@ describe("Script generator", function() {
     const type = 'click';
     const keywords = 'test etsy';
     const url = 'https://openapi.etsy.com/v2/listings/active.js?includes=MainImage&callback=handleData&api_key=2z5arsdpfe5py1mcc5welkm2&keywords=test%20etsy&limit=25';
-    expect(etsy.generateScript(type, keywords)).toEqual(url);
+    expect(etsy.testObj.generateScript(type, keywords)).toEqual(url);
   });
 
   it("generates script src for loading next 25 results", function() {
@@ -18,14 +18,14 @@ describe("Script generator", function() {
     const keywords = 'test etsy';
     const resultsLength = 25;
     const url = 'https://openapi.etsy.com/v2/listings/active.js?includes=MainImage&callback=handleData&api_key=2z5arsdpfe5py1mcc5welkm2&keywords=test%20etsy&offset=25&limit=25';
-    expect(etsy.generateScript(type, keywords, resultsLength)).toEqual(url);
+    expect(etsy.testObj.generateScript(type, keywords, resultsLength)).toEqual(url);
   });
 
   it("generates script src for loading page load results", function() {
     const type = 'load';
     const keywords = '';
     const url = 'https://openapi.etsy.com/v2/listings/active.js?includes=MainImage&callback=handleData&api_key=2z5arsdpfe5py1mcc5welkm2&limit=25';
-    expect(etsy.generateScript(type, keywords)).toEqual(url);
+    expect(etsy.testObj.generateScript(type, keywords)).toEqual(url);
   });
 });
 
@@ -40,6 +40,6 @@ describe("Results HTML generator", function() {
     };
     const index = 0;
     const html = '<li data-index="0"><div class="tile_thumb" style="background-image:url(https://img1.etsystatic.com/181/0/14398010/il_fullxfull.1513953521_60wa.jpg)"></div><div class="price">$5.50</div><div class="tile_title" onclick="etsy.loadEtsyPage(0)">Henry Glass Fabrics - Botanica Blooms Floral by Color Principle<div class="view_product">View Product</div></div></li>';
-    expect(etsy.generateResultsHTML(result, index)).toEqual(html);
+    expect(etsy.testObj.generateResultsHTML(result, index)).toEqual(html);
   });
 });
