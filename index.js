1c27e984c8b3041f8f749d3c594930e4b1ba0714
bla211 <brad.louis.alexander@gmail.com>
2018-07-31 09:30:07 -0400

    styling changes to bookmarking and searching

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ etsy.search = (type) => {
   let offsetString = '';
   if(type === 'click'){
     etsy.searchResults = [];
-    document.getElementById('results_list').innerHTML = '<img id="loading_animation" src="https://digitalsynopsis.com/wp-content/uploads/2016/06/loading-animations-preloader-gifs-ui-ux-effects-10.gif"/>';
+    document.getElementById('results_list').innerHTML = '<img id="loading_animation" src="https://josephdoughertyblog.files.wordpress.com/2014/09/loading-animation-white.gif"/>';
     etsy.keywords = document.getElementById('search_box').value;
     keywordString = '&keywords=' + encodeURI(etsy.keywords);
   }
   else if(type === 'load'){
-    document.getElementById('results_list').innerHTML = '<img id="loading_animation" src="https://digitalsynopsis.com/wp-content/uploads/2016/06/loading-animations-preloader-gifs-ui-ux-effects-10.gif"/>';
+    document.getElementById('results_list').innerHTML = '<img id="loading_animation" src="https://josephdoughertyblog.files.wordpress.com/2014/09/loading-animation-white.gif"/>';
     etsy.searchResults = [];
   }
   else if(type === 'next'){
